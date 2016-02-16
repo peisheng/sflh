6e6c37e4b0cb1e633feb4f2f172e4a42bea5cb44
ilovemeyou2000@163.com <ilovemeyou2000@163.com>
2016-02-16 21:52:36 +0800

    sflh

---
+++ data/conf/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'sflh',
+    'DB_USER' => 'root',
+    'DB_PWD' => '840904aA',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'sflh_',
+    //密钥
+    "AUTHCODE" => '15dXHk7SSb0l5LWHvM',
+    //cookies
+    "COOKIE_PREFIX" => 'tlO4kv_',
+);
